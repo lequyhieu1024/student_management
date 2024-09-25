ea3dcbb743b49b09066c06dc711444f49a018321
Lê Hiếu <136102856+lequyhieu1024@users.noreply.github.com>
2024-09-25 10:40:25 +0700

    release 25092024

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -176,6 +176,7 @@ class StudentController extends Controller
 
     public function updateScores(ScoreFormRequest $request, $studentId)
     {
+        dd($request->all());
         $this->studentRepository->updateScore($studentId, $request->scores);
         return redirect()->back()->with('success', __('Updated Successfully'));
     }
